46941e1a86e5e90ba5b4c2b96f5dab63ce4d98c7
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-10 19:42:26 +0530

    brandswiper update

---
+++ lk.ijse.gdse/Assests/JS/main.js
@@ -0,0 +1,27 @@
+// ======================= Show Menu ==========================
+// ======================= Menu Show ==========================
+// ======================= Menu Hiden =========================
+// ======================= Scroll Header ======================
+
+
+// ======================= Brand Swiper ========================
+var brandSwiper = new Swiper(".brand-swiper", {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
+
+// ======================= Anime Swiper ======================
