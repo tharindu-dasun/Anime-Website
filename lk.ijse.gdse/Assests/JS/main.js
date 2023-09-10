6dc5121b015fc9d3183b2afcbb78177f66c3da78
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-10 23:13:19 +0530

    anime swiper update 0.1

--- lk.ijse.gdse/Assests/JS/main.js
+++ lk.ijse.gdse/Assests/JS/main.js
@@ -29,3 +29,25 @@ var brandSwiper = new Swiper(".brand-swiper", {
 });
 
 // ======================= Anime Swiper ======================
+var animeSwiper = new Swiper(".anime-swiper", {
+    spaceBetween: 24,
+    loop: true,
+    breakpoints: {
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 24,
+        },
+    },
+});
