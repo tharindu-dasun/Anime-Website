2b14a27dfa3769e1510a9575dddcfcbe4783d685
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-09-11 18:20:57 +0530

    js part update

--- lk.ijse.gdse/Assests/JS/main.js
+++ lk.ijse.gdse/Assests/JS/main.js
@@ -1,6 +1,21 @@
 // ======================= Show Menu ==========================
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
 // ======================= Menu Show ==========================
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    });
+}
+
 // ======================= Menu Hiden =========================
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 // ======================= Scroll Header ======================
 function scrollHeader(){
     const header = document.getElementById('header')
